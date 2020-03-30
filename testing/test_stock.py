94f69867de570d9a1a6a0eabcb6a4b0183b28000
backster82 <AqXt6t0tlMsAllMmPpEh>
2020-03-31 01:25:23 +0200

    removed accidently commited testing folder

--- testing/test_stock.py
+++
@@ -1,7 +0,0 @@
-import finanzen_fundamentals
-
-stock = finanzen_fundamentals.stock.Stock("deutsche_telekom")
-print(stock.get_fundamentals())
-
-for col in stock.get_fundamentals().columns:
-    print(col)
